970127f224379a8f894d68251d7077c9df885926
alanpost <alanpost@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2010-12-02 23:35:02 +0000

    jbogenturfa'i: rename declare.scm -> chicken-declare.scm
    This is a chicken-specific file, so give it a chicken- prefix.
    
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/jbogenturfahi/trunk@21781 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- chicken-cmd.scm
+++ chicken-cmd.scm
@@ -17,7 +17,7 @@
 ;;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ;;;;
 
-(include "declare.scm")
+(include "chicken-declare.scm")
 
 ;(use utf8)
 (use srfi-28)       ; Basic Format Strings
