c6476dfac2a2b6f54d861dabc4e9fd9954467c69
alanpost <alanpost@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2010-12-13 15:31:33 +0000

    jbogenturfa'i: add -pickly, remove -O2 from csc-flags
    -O2 is sufficiently handled by (declare ...), and -picky is a compiler
    option from the experimental branch.
    
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/jbogenturfahi/trunk@21963 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

---
+++ chicken-ext-jbogensamselpla.scm
@@ -0,0 +1,147 @@
+;;;;
+;;;; jbogenturfahi - lo lojbo ke pe'a jajgau ratcu ke'e genturfa'i
+;;;;               `-> A Lojban grammar parser
+;;;;
+;;;; Copyright (c) 2010 ".alyn.post." <alyn.post@lodockikumazvati.org>
+;;;;
+;;;; Permission to use, copy, modify, and/or distribute this software for any
+;;;; purpose with or without fee is hereby granted, provided that the above
+;;;; copyright notice and this permission notice appear in all copies.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;;;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;;;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;;;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;;;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;;;;
+
+(module jbogensamselpla
+  (A
+   BAI
+   BAhE
+   BE
+   BEI
+   BEhO
+   BIhE
+   BIhI
+   BO
+   BOI
+   BU
+   BY
+   CAhA
+   CAI
+   CEI
+   CEhE
+   CO
+   COI
+   CU
+   CUhE
+   DAhO
+   DOI
+   DOhU
+   FA
+   FAhA
+   FAhO
+   FEhE
+   FEhU
+   FIhO
+   FOI
+   FUhA
+   FUhE
+   FUhO
+   GA
+   GAhO
+   GEhU
+   GI
+   GIhA
+   GOI
+   GOhA
+   GUhA
+   I
+   JA
+   JAI
+   JOhI
+   JOI
+   KE
+   KEhE
+   KEI
+   KI
+   KOhA
+   KU
+   KUhE
+   KUhO
+   LA
+   LAU
+   LAhE
+   LE
+   LEhU
+   LI
+   LIhU
+   LOhO
+   LOhU
+   LU
+   LUhU
+   MAhO
+   MAI
+   ME
+   MEhU
+   MOhE
+   MOhI
+   MOI
+   NA
+   NAI
+   NAhE
+   NAhU
+   NIhE
+   NIhO
+   NOI
+   NU
+   NUhA
+   NUhI
+   NUhU
+   PA
+   PEhE
+   PEhO
+   PU
+   RAhO
+   ROI
+   SA
+   SE
+   SEI
+   SEhU
+   SI
+   SOI
+   SU
+   TAhE
+   TEhU
+   TEI
+   TO
+   TOI
+   TUhE
+   TUhU
+   UI
+   VA
+   VAU
+   VEI
+   VEhO
+   VEhA
+   VIhA
+   VUhO
+   VUhU
+   XI
+   Y
+   ZAhO
+   ZEhA
+   ZEI
+   ZI
+   ZIhE
+   ZO
+   ZOI
+   ZOhU)
+
+(import chicken)
+(import scheme)
+
+(include "samselpla.scm"))
---
+++ samselpla.scm
@@ -0,0 +1,388 @@
+;;;;
+;;;; jbogenturfahi - lo lojbo ke pe'a jajgau ratcu ke'e genturfa'i
+;;;;               `-> A Lojban grammar parser
+;;;;
+;;;; Copyright (c) 2010 ".alyn.post." <alyn.post@lodockikumazvati.org>
+;;;;
+;;;; Permission to use, copy, modify, and/or distribute this software for any
+;;;; purpose with or without fee is hereby granted, provided that the above
+;;;; copyright notice and this permission notice appear in all copies.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;;;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;;;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;;;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;;;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;;;;
+
+;;
+;; selma'o
+;;
+
+(define (A . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BAI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BAhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BEhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BIhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BIhI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (BY . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CAhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CAI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CEhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (COI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (CUhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (DAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (DOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (DOhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FAhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FEhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FIhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FUhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FUhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (FUhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GIhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GOhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (GUhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (I . rodalerfu)
+  (apply string rodalerfu))
+
+(define (JA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (JAI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (JOhI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (JOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KEhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KOhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KUhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (KUhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LAU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LAhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LIhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LOhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LOhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (LUhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MAI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ME . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MOhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MOhI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (MOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NAI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NAhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NAhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NIhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NIhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NUhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NUhI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (NUhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (PA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (PEhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (PEhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (PU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (RAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ROI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (SU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TAhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TEhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TUhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (TUhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (UI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VAU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VEhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VEhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VIhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VUhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (VUhU . rodalerfu)
+  (apply string rodalerfu))
+
+(define (XI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (Y . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZAhO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZEhA . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZEI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZIhE . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZO . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZOI . rodalerfu)
+  (apply string rodalerfu))
+
+(define (ZOhU . rodalerfu)
+  (apply string rodalerfu))
